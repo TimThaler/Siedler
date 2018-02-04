72a425862f16d540a60f5844f82721306df3986c
tim <tim_schuenemann@gmx.de>
2018-02-04 19:13:50 +0100

    using dbc as object an not static class

--- src/spiel/Spielbrett.java
+++ src/spiel/Spielbrett.java
@@ -7,21 +7,22 @@ import interfaces.Konstanten;
 
 public class Spielbrett {
 	public static Spielbrett instance = null;
-
+	public DatabaseConnector dbc = null;
 	private Feld[] felder;
 	
-	public static Spielbrett getInstance(int anzFelder)
+	public static Spielbrett getInstance(int anzFelder, DatabaseConnector dbc)
 	{
 		if(instance == null)
 		{
-			instance = new Spielbrett(anzFelder);
+			instance = new Spielbrett(anzFelder, dbc);
 		}
 		return instance;
 	}
 	
-	private Spielbrett(int anzFelder)
+	private Spielbrett(int anzFelder, DatabaseConnector dbc)
 	{
 		Random r = new Random();
+		this.dbc = dbc;
 		
 		felder = new Feld[anzFelder];
 		for(int i = 0; i < felder.length; i++)
@@ -31,23 +32,21 @@ public class Spielbrett {
 			felder[i] = feld;
 		}
 		
-        DatabaseConnector.connectToSiedlerDatabase();
-        DatabaseConnector dbc = new DatabaseConnector();
         if(dbc.tableExists(Struktur.FIELD)){
-        	DatabaseConnector.clearTable(Struktur.FIELD);
+        	dbc.clearTable(Struktur.FIELD);
         }else {
         	dbc.createTable(Struktur.FIELD);
         }
         dbc.tableExists(Struktur.CORNER);
         //DatabaseConnector.createTable(Struktur.FIELD);
         //DatabaseConnector.createTable(Struktur.Knoten);
-		DatabaseConnector.createTable(Struktur.CORNER);
+		//DatabaseConnector.createTable(Struktur.CORNER);
 		
 	
 			//int pk = DatabaseConnector.addField();
 			for(int x = 0; x <5; x++)
 			{
-				DatabaseConnector.addCorner(1);
+				dbc.addCorner(1);
 			}
 				
 	}
