fc97b9010f787d0be8c89118cc9e729cf51716cb
tim <tim_schuenemann@gmx.de>
2018-02-04 19:05:51 +0100

    changed class to non-static class

--- src/spiel/DatabaseConnector.java
+++ src/spiel/DatabaseConnector.java
@@ -13,13 +13,18 @@ import interfaces.Konstanten;
 
 public class DatabaseConnector {
 	Connection c = null;
-
+	Statement stmt = null;
+	DatabaseMetaData dmd = null;
+	String sql = null;
+	ResultSet rs = null;
+	
 	public DatabaseConnector(){
 		try{
 			this.c = DriverManager
 					.getConnection(Konstanten.POSTGRES_URL,
 							Konstanten.POSTGRES_USER,
 							Konstanten.POSTGRES_PASSWORD);
+			System.out.println("[***] Opened database " +  Konstanten.POSTGRES_URL + " successful");
 		}catch(Exception e){
 			e.printStackTrace();
 			System.err.println(e.getClass().getName()+": "+e.getMessage());
@@ -27,36 +32,22 @@ public class DatabaseConnector {
 		}
 	}
 	
-	public static void clearTable(Struktur struktur) {
-		Connection c = null;
-		Statement stmt = null;
-		String sql = "DELETE  FROM " + struktur.toString().toLowerCase();
+	public void clearTable(Struktur struktur) {
+		sql = "DELETE  FROM " + struktur.toString().toLowerCase();
         
 		try {
-			c = DriverManager
-					.getConnection(Konstanten.POSTGRES_URL,
-							Konstanten.POSTGRES_USER,
-							Konstanten.POSTGRES_PASSWORD);
-			
 			stmt = c.createStatement();
 			stmt.executeUpdate(sql);
 			System.out.println("[***] Table "+ struktur.toString() + " cleared");
 		}catch (Exception ex) {
 			ex.printStackTrace();
-			}finally {
-			   
-			    try { stmt.close(); } catch (Exception e) { /* ignored */ }
-			    try { c.close(); } catch (Exception e) { /* ignored */ }
 			}
-
-
 	}
+	
 	public boolean tableExists(Struktur struktur){
 		try {
-			DatabaseMetaData dmd = null;
 			dmd = c.getMetaData();
 			String tableName = struktur.toString().toLowerCase();
-
 			ResultSet rs = dmd.getTables(null, null,"%", new String[] {"TABLE"});
 			while (rs.next()){			
 				if(rs.getString(3).equals(tableName)){
@@ -69,47 +60,18 @@ public class DatabaseConnector {
 		return false;
 	}
 
-	public static void connectToSiedlerDatabase()
-	{
-		try {
-			Connection c = DriverManager
-					.getConnection(Konstanten.POSTGRES_URL,
-							Konstanten.POSTGRES_USER,
-							Konstanten.POSTGRES_PASSWORD);
-			System.out.println("[***] Opened database successfully");
-
-
-
-			c.close();			   
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.err.println(e.getClass().getName()+": "+e.getMessage());
-			System.exit(0);
-		}
-	}
-
-	public static void 	 createTable(Struktur struktur) {
+	public void createTable(Struktur struktur) {
 		String tableName = null;
 		String setupParams = null;
 
-		Connection c = null;
-		Statement stmt = null;
-		DatabaseMetaData dbmd = null;
-		ResultSet rs = null;
-
 		try {	
-			c = DriverManager
-					.getConnection(Konstanten.POSTGRES_URL,
-							Konstanten.POSTGRES_USER,
-							Konstanten.POSTGRES_PASSWORD
-							);
-			
-			dbmd = c.getMetaData();
+			dmd = c.getMetaData();
 		
 			switch (struktur){
 				case FIELD:
 					tableName = "feld";
 					setupParams = Konstanten.FIELD_TABLE_SETUP;
+					sql = "Create table field (ID SERIAL UNIQUE," + setupParams + ");";  
 					break;
 				case Knoten:
 					tableName = "knoten";
@@ -118,50 +80,16 @@ public class DatabaseConnector {
 				case CORNER:
 					tableName = "corner";
 					setupParams = Konstanten.ECKENTABLESETUP;
+					sql = "Create table ecke (ID SERIAL UNIQUE, field_id integer REFERENCES field(id));" ;
 					break;
 			}
-			
-			rs =  dbmd.getTables(null, null, tableName.toLowerCase(), new String[] {"TABLE"});
 			stmt = c.createStatement(); 
-			String sql = null;
-			
-			if (rs.next()) {	
-				System.out.println("[***] Table " + tableName + " exists");
-						
-                //System.out.println(rs.getString("TABLE_NAME"));
-                //sql = "DELETE  FROM " + tableName;
-                stmt.executeUpdate(sql); 
-                //System.out.println(sql);
-
-			}else{
-                System.out.println("[***] Table " + tableName + " does not exist"); 
-                if (struktur == Struktur.CORNER)
-                {
-                	sql = "Create table ecke (ID SERIAL UNIQUE, field_id integer REFERENCES field(id));" ;
-                }else if (struktur ==Struktur.FIELD) {
-                	sql = "Create table field (ID SERIAL UNIQUE," + setupParams + ");";       
-                }
-                System.out.println(sql + " HELLO ");
-                	stmt.executeUpdate(sql); 	
-    			
-			}
-			System.out.println("[***] Table " + tableName + " created"); 
-			//System.out.println(sql);
-			
-		}/*atch(SQLException ex) {
-			
-			System.out.print("Hallo");
-		}*/
-		catch(SQLException ex){
+        	stmt.executeUpdate(sql); 
+			System.out.println("[***] Table " + tableName + " created"); 	
+		}catch(SQLException ex){
 			ex.printStackTrace();
 			System.err.println(ex.getClass().getName() + ": " + ex.getMessage());
 			System.exit(0);
-		}finally {
-			try {								
-				if(stmt != null) stmt.close();
-				if(c != null) c.close();
-				if(rs != null) rs.close();
-			}catch(Exception e) {}
 		}
 	}
 
@@ -194,16 +122,12 @@ public class DatabaseConnector {
 	   }catch(Exception ex){
 		    System.err.println(ex.getClass().getName() + " " + ex.getMessage());
             System.exit(0);
-	   }finally {
-		  try {
-			  if(stmt != null) stmt.close();
-			  if(c != null) c.close();
-		  }catch(Exception e) {}
 	   }
+		
 		return primaryKey;
 	}
 
-	public static int addCorner(int pkField) {
+	public int addCorner(int pkField) {
 		Connection c = null;
 		Statement stmt = null;
 		try {			   
@@ -222,12 +146,16 @@ public class DatabaseConnector {
 	   }catch(Exception ex){
 		    System.err.println(ex.getClass().getName() + " " + ex.getMessage());
             System.exit(0);
-	   }finally {
-		  try {
-			  if(stmt != null) stmt.close();
-			  if(c != null) c.close();
-		  }catch(Exception e) {}
 	   }
 		return 0;		
+	}
+	
+	public void close()
+	{
+		try {								
+			if(stmt != null) stmt.close();
+			if(c != null) c.close();
+			if(rs != null) rs.close();
+		}catch(Exception e) {}
 	}
 }
