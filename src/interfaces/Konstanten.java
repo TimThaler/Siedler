2aae00e607312c9cb0519cf09433d662f08383ed
tim <tim_schuenemann@gmx.de>
2018-02-07 00:16:27 +0100

    clean up konstanten interface; using whole sql string for creating class

--- src/interfaces/Konstanten.java
+++ src/interfaces/Konstanten.java
@@ -5,32 +5,32 @@ public interface Konstanten {
 	public static final String POSTGRES_USER = "postgres";
 	public static final String POSTGRES_PASSWORD = "123";
 	
+	public static final int ANZ_ECKEN_PRO_FELD = 6;
 	public static final int MAX_POOL_SIZE = 5;
 	
 	public static final int MAX_KARTEN_BEI_RAEUBER = 7;
 	public static final int GESAMT_PUNKTE_ZUM_GEWINNEN = 12;
-	public static final int ANZ_FELDER_AUF_SPIELBRETT = 9;
+	public static final int ANZ_FELDER_AUF_SPIELBRETT = 1;
+	
+	
 	public static final String FIELD_TABLE_SETUP = 
 			" Name TEXT NOT NULL," +
-			" Rohstoff TEXT NOT NULL" 
-					;
+			" Rohstoff TEXT NOT NULL" ;	
 	
-	public static final String KNOTENTABLESETUP = 
-			" Name TEXT NOT NULL," +
-			" Rohstoff TEXT NOT NULL";
+	public static final String CORNER_TABLE_SETUP =
+			"Create table corner (ID SERIAL UNIQUE,"
+			+ "field_id integer REFERENCES field(id)"
+			+ ");";
 	
-	public static final String ECKENTABLESETUP = 
-			" feld_id integer REFERENCES field(id)";
-					
+	public static final String EDGE_TABLE_SETUP = 
+			"Create table edge (ID SERIAL UNIQUE, "
+			+ "field_id integer REFERENCES field(id), "
+			+ "corner_1_id integer REFERENCES corner(id) NOT NULL,"
+			+ "corner_2_id integer REFERENCES corner(id) NOT NULL "
+			+ ");";	
+	
+	public static final String NODE_TABLE_SETUP = "";
 	
 	public static final String POSTGRES_CREATE_TABLE_PREFIX =
-			" (ID SERIAL PRIMARY KEY,";
-	
-	public static final String POSTGRES_CREATE_TABLE_SUFFFIX =
-			");";
-
-	
-	
-			
-	
+			" (ID SERIAL PRIMARY KEY,";	
 }
